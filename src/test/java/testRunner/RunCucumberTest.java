49d4eb05af6f2c81696646748b62be5e9a814034
Dika Brenda <dika.brenda@paques.id>
2022-05-18 16:15:53 +0700

    added cucumber runner

---
+++ src/test/java/testRunner/RunCucumberTest.java
@@ -0,0 +1,22 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {
+                "classpath:features",
+        },
+        plugin = {
+                "pretty",
+                "rerun:rerun/failed_scenarios.txt",
+                "json:target/cucumber.json",
+        },
+        glue = {
+                "stepdefinitions"
+        })
+
+public class RunCucumberTest {
+}
