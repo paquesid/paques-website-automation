74b6d30ef6b161d85ceec6537f95e1a053feeb47
Dika Brenda <dika.brenda@paques.id>
2022-05-20 11:29:00 +0700

    create scenario forgot password

--- src/test/java/helper/CucumberPages.java
+++ src/test/java/helper/CucumberPages.java
@@ -1,21 +1,23 @@
 package helper;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
+import pages.ForgotPasswordPage;
 import pages.LoginPage;
 
 public class CucumberPages {
     
-    WebDriver driver;
+    private final WebDriver driver;
 
     public CucumberPages(WebDriver driver){
         this.driver = driver;
-        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 20), this);
     }
 
     public LoginPage loginPage(){
         return new LoginPage(driver);
+    }
+
+    public ForgotPasswordPage forgotPasswordPage(){
+        return new ForgotPasswordPage(driver);
     }
 }
