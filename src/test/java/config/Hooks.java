b8f2cfcb7117236516649833521973b7f12f200a
Dika Brenda <dika.brenda@paques.id>
2022-05-20 11:24:26 +0700

    refactor cucumber hooks

--- src/test/java/config/Hooks.java
+++ src/test/java/config/Hooks.java
@@ -1,9 +1,7 @@
 package config;
 
-import static helper.Constant.*;
-
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.util.Objects;
 
 import org.apache.commons.io.FileUtils;
@@ -13,31 +11,26 @@ import org.openqa.selenium.WebDriver;
 
 import io.cucumber.java8.En;
 import io.cucumber.java8.Scenario;
-import helper.CucumberPages;
 import helper.TestInstrument;
 
 public class Hooks extends TestInstrument implements En {
 
     protected static WebDriver driver;
-    public CucumberPages Website;
     private static String scenarioName;
     String path = System.getProperty("user.dir") + "/screenshot_failed";
     File srcFile = null;
     byte[] screenshot = new byte[0];
 
-    public Hooks(){
-        
+    public Hooks() {
+
         Before(0, () -> {
             setupBrowser();
             pageObj();
-            Runtime.getRuntime().addShutdownHook(new Thread(this::tearDown));
         });
 
         Before(1, (Scenario scenario) -> {
-            
             scenarioName = scenario.getName();
-
-            if(scenario.isFailed()) {
+            if (scenario.isFailed()) {
                 srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
                 screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
                 File imageFile = new File(path + scenarioName + ".png");
@@ -45,7 +38,7 @@ public class Hooks extends TestInstrument implements En {
                     scenario.attach(screenshot, "image/png", scenarioName);
                     FileUtils.copyFile(Objects.requireNonNull(srcFile), imageFile);
                     System.out.println("Image has been taken");
-                } catch (Exception e) {
+                } catch (IOException e) {
                     System.out.println("Exception while taking screenshot : " + e.getMessage());
                 }
             }
@@ -53,22 +46,6 @@ public class Hooks extends TestInstrument implements En {
 
         After(0, () -> {
             tearDown();
-            System.out.println("teardown");
         });
-    }
-
-    public WebDriver setupBrowser() throws MalformedURLException {
-        driver = Base.startApplication(CHROME, PRODUCTION);
-        return driver;
-    }
-
-    public void pageObj(){
-        Website = new CucumberPages(driver);
-    }
-
-    public void tearDown(){
-        if (driver != null){
-            driver.quit();
-        }
     }
 }
