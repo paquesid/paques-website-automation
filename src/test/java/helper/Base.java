23028322fae33555a859e5e4dde11b7d8df1517b
Dika Brenda <dika.brenda@paques.id>
2022-05-22 22:09:32 +0700

    Set Accept Non SSL

--- src/test/java/helper/Base.java
+++ src/test/java/helper/Base.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Base {
@@ -61,7 +63,8 @@ public class Base {
     private static void getChromeDriver(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito", "--headless", "--window-size=1325x744"); // "--headless", "--window-size=1325x744"
+        options.addArguments("--incognito"); // "--headless", "--window-size=1325x744"
+        options.setAcceptInsecureCerts(true);
         driver = new ChromeDriver(options);
     }
 
