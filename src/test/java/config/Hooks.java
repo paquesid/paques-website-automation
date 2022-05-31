cbc3ca707323727d3c71cdba54a5ac084af28731
Dika Brenda <dika.brenda@paques.id>
2022-05-31 14:32:01 +0700

    added scenario name

--- src/test/java/config/Hooks.java
+++ src/test/java/config/Hooks.java
@@ -1,6 +1,16 @@
 package config;
 
 import io.cucumber.java8.En;
+import io.cucumber.java8.Scenario;
+import utils.LogUtils;
+
+import java.io.File;
+import java.util.Objects;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import helper.TestInstrument;
 
 public class Hooks extends TestInstrument implements En {
@@ -10,6 +20,24 @@ public class Hooks extends TestInstrument implements En {
         Before(0, () -> {
             setupBrowser();
             pageObj();
+        });
+
+        After(1, (Scenario scenario) -> {
+            scenarioName = scenario.getName();
+            
+            if(scenario.isFailed()){
+                String path = System.getProperty("user.dir") + "/screenshots_failed/";
+                File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                File imageFile = new File(path + scenarioName + ".png");
+                try {
+                    scenario.attach(screenshot, "image/png", "failed screenshot");
+                    FileUtils.copyFile(Objects.requireNonNull(srcFile), imageFile);
+                    LogUtils.info("Screenshot has taken");
+                } catch (Exception e) {
+                    LogUtils.error("Exception while taking screenshot", e);
+                }
+            }
         });
 
         After(0, () -> {
