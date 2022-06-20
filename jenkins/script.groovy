f259a919c1c7123261b5985f5d2e0f1e6a76432e
Dika Brenda <dika.brenda@paques.id>
2022-06-20 11:20:15 +0700

    add Jenkinsfile to integration jenkins

---
+++ jenkins/script.groovy
@@ -0,0 +1,47 @@
+def CheckoutSCM(){
+    bat "docker run -p 3000:3000 dikabrenda/cypress_project:v3 ."
+}
+
+def AtBranch() {
+    BRANCH_NAME == 'dev' && CODE_CHANGES = true
+}
+
+def PopulateEnv(){
+    bat "cp env/sample.env .env"
+}
+
+def TestApps(){
+    bat 'mvn clean verify -Dcucumber.filter.tags="@regression"'
+}
+
+def InitialSession(){
+    bat "docker compose up"
+}
+
+def RemoveSession(){
+    bat "docker compose down"
+}
+
+def PublishReport(){
+    bat "cucumber buildStatus: 'null', 
+    customCssFiles: '', 
+    customJsFiles: '', 
+    failedFeaturesNumber: -1, 
+    failedScenariosNumber: -1, 
+    failedStepsNumber: -1, 
+    fileIncludePattern: '**/*.json', 
+    pendingStepsNumber: -1, 
+    skippedStepsNumber: -1, 
+    sortingMethod: 'ALPHABETICAL', 
+    undefinedStepsNumber: -1"
+}
+
+def CucumberReport(){
+    cucumber '**/cucumber.json'
+}
+
+def SendToTelegram(){
+    echo "send to telegram.."
+}
+
+return this;
