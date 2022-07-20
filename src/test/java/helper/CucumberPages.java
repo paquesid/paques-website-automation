668edc77c34c0fab56d624cbb779b6c730d4b70b
dikabrenda <dika.brenda@paques.id>
2022-07-20 14:16:53 +0700

    remove telegrampages

--- src/test/java/helper/CucumberPages.java
+++ src/test/java/helper/CucumberPages.java
@@ -8,7 +8,6 @@ import pages.HomePage;
 import pages.LoginPage;
 import pages.UsersPage;
 import pages.onBoardingPage;
-import telegram.TelegramStep;
 
 /**
  * Created by Dika Brenda Angkasa on 23/05/2022
@@ -44,9 +43,5 @@ public class CucumberPages {
 
     public DataProcessingPage dataProcessingPage(){
         return new DataProcessingPage(driver);
-    }
-
-    public TelegramStep telegramStep(){
-        return new TelegramStep(driver);
     }
 }
